86c81b5af61ea1eab61fd42e4f709c824a61cbdd
inji0212 <in17100170@kakao.com>
2024-10-18 16:59:38 +0900

    chore home 기본으로 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,5 @@
-import Tag from '@/components/shared/Tag';
-
 export default function Home() {
   return (
-    <div className="mx-5 flex flex-col gap-4">
-      <div className="relative h-[180px] w-[343px] rounded-3xl border-2 mobile:rounded-tr-[0px]">
-        <Tag Type="default" className="absolute right-0 top-0" Hour={9} />
-        <div className="absolute inset-0 z-[-1] rounded-3xl bg-black mobile:rounded-tr-[0px]" />
-      </div>
-      <div className="relative h-[180px] w-[343px] rounded-3xl border-2">
-        <Tag Type="detail" className="absolute right-0 top-0" Hour={2} />
-        <div className="absolute inset-0 z-[-1] rounded-3xl bg-black" />
-      </div>
-    </div>
+    <div className="sm:p-20 grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-black p-8 pb-20 font-[family-name:var(--font-geist-sans)] text-gray-300"></div>
   );
 }
