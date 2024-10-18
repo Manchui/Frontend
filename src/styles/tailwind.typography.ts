2b5d6203a39d0da53cc867dc82cd6653b5eb2ea1
eundonghyeok <mephisto7191@naver.com>
2024-10-18 17:07:15 +0900

    tailwind.config 분할관리

---
+++ src/styles/tailwind.typography.ts
@@ -0,0 +1,19 @@
+const typography = {
+  fontSize: {
+    'xs': ['12px', '18px'],
+    'sm': ['13px', '22px'],
+    'md': ['14px', '24px'],
+    'lg': ['16px', '26px'],
+    '2lg': ['18px', '26px'],
+    'xl': ['20px', '32px'],
+    '2xl': ['24px', '32px'],
+    '3xl': ['32px', '42px'],
+  },
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+};
+export default typography;
