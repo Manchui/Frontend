695a4bff266d5b8c54053a4f8e2ce280dd583522
inji0212 <in17100170@kakao.com>
2024-10-18 16:55:27 +0900

    chore twmerge 적용

--- src/components/shared/Tag/index.tsx
+++ src/components/shared/Tag/index.tsx
@@ -1,24 +1,27 @@
 import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
 /**
 Tag 컴포넌트
 * @description 오늘 마감되는 모임의 마감시간을 알려주는 컴포넌트
 * @param {number} [Hour=''] - 오늘 마감 시간
 * @param {'default' | 'detail'} [Type='default'] - 버튼의 타입
+* @param {string} [className=''] - tailwind.css 적용 가능
 * @todo 반응형 작업하였고 추후 컬러만 변경
 */
 
 type TTagProps = {
   Hour: number;
   Type: 'default' | 'detail';
+  className?: string;
 };
 
 const baseStyles = 'flex items-center gap-1 h-8 bg-orange-600 py-1 pl-2 pr-4 rounded-bl-xl w-[123px]';
 
-export default function Tag({ Type, Hour }: TTagProps) {
+export default function Tag({ Type, Hour, className }: TTagProps) {
   const variantStyles = Type === 'default' ? ' rounded-tr-[22px] mobile:rounded-tr-[0px] mobile:w-[117px] mobile:pr-2' : 'rounded-tr-[22px]';
 
   return (
-    <div className={`${baseStyles} ${variantStyles}`}>
+    <div className={twMerge(baseStyles, variantStyles, className)}>
       <Image src="/icons/alarm.svg" width={24} height={24} alt="alarm" />
       <p className="text-xs leading-4 text-white">오늘 {Hour}시 마감</p>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,15 @@ import Tag from '@/components/shared/Tag';
 
 export default function Home() {
   return (
-    <div className="mx-5 flex space-x-3">
-      <Tag Type="default" Hour={9} />
-      <Tag Type="detail" Hour={2} />
+    <div className="mx-5 flex flex-col gap-4">
+      <div className="relative h-[180px] w-[343px] rounded-3xl border-2 mobile:rounded-tr-[0px]">
+        <Tag Type="default" className="absolute right-0 top-0" Hour={9} />
+        <div className="absolute inset-0 z-[-1] rounded-3xl bg-black mobile:rounded-tr-[0px]" />
+      </div>
+      <div className="relative h-[180px] w-[343px] rounded-3xl border-2">
+        <Tag Type="detail" className="absolute right-0 top-0" Hour={2} />
+        <div className="absolute inset-0 z-[-1] rounded-3xl bg-black" />
+      </div>
     </div>
   );
 }
