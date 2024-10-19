b8e76db86dc00b01a3ecd9504acb5a6b8510aaa7
eundonghyeok <mephisto7191@naver.com>
2024-10-19 14:18:40 +0900

    Refactor: 코드수정

--- src/components/shared/Modal/index.tsx
+++ src/components/shared/Modal/index.tsx
@@ -2,7 +2,7 @@ import { type ReactNode, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 type ModalProps = {
-  children: (handleClose: () => void) => ReactNode;
+  children: React.ReactNode;
   isOpen: boolean;
   onClose: () => void;
 };
@@ -12,7 +12,7 @@ type ModalProps = {
  *
  * @property {boolean} isOpen - Modal Open 여부
  * @property {() => void} onClose - Modal Close 함수
- * @property {(handleClose: () => void) => ReactNode} children - render props 방식으로, 모달 내부의 콘텐츠를 렌더링하는 함수. handleClose를 인자로 받아서 모달을 닫을 수 있음
+ * @property {React.ReactNode} children - render props 방식으로, 모달 내부의 콘텐츠를 렌더링하는 함수. handleClose를 인자로 받아서 모달을 닫을 수 있음
  */
 
 export default function Modal({ isOpen, onClose, children }: ModalProps) {
@@ -38,7 +38,7 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
 
   return ReactDOM.createPortal(
     <div className={`fixed inset-0 z-[9999] flex items-center justify-center bg-black/30 ${animation}`} onClick={handleBackgroundClick}>
-      <div className="rounded-xl bg-white drop-shadow-2xl">{children(handleClose)}</div>
+      <div className="rounded-xl bg-white drop-shadow-2xl">{children}</div>
     </div>,
     document.body,
   );
