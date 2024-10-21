3cc917e66d2b01c1c0631aaa99604d4111dbd7e4
eundonghyeok <mephisto7191@naver.com>
2024-10-21 15:10:54 +0900

    Fix: Calendar React Hooks 사용

--- src/components/shared/Calendar/RenderCalendar/index.tsx
+++ src/components/shared/Calendar/RenderCalendar/index.tsx
@@ -1,52 +1,53 @@
-interface RenderCalendarProps {
+interface CalendarGridProps {
   currentDate: Date;
-  handleDateSelection: (date: string) => void;
+  onDateSelect: (date: string) => void;
   selectedDate: string | null;
 }
 
-export default function RenderCalendar({ currentDate, handleDateSelection, selectedDate }: RenderCalendarProps) {
-  const days = [];
+export default function CalendarGrid({ currentDate, onDateSelect, selectedDate }: CalendarGridProps) {
+  // 현재 달의 날짜들 계산
+  const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(); // 현재 달의 마지막 날
+  const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay(); // 현재 달의 1일의 요일
+  const prevMonthDays = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate(); // 전달의 마지막 날
 
-  const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(); // 31 다음달 마지막일 (현재 달의 마지막 날)
-  const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay(); // 2 이번달 1일의 요일 (화요일) (현재 달의 1일이 무슨 요일인지 구함)
-  const prevMonthDays = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate(); // 30 전달 마지막 일 (전달의 마지막날을 구함)
-
-  // 현재 달의 첫 번째 요일이 시작되기 전에 표시할 전달(이전 달)의 날짜들을 화면에 출력하기 위한 코드입니다.
-  for (let i = firstDayOfMonth - 1; i >= 0; i -= 1) {
+  // 전달의 날짜 계산
+  const prevDays = Array.from({ length: firstDayOfMonth }, (_, i) => {
     const day = prevMonthDays - i;
-
-    days.push(
-      <div key={`prev-${day}`} className="py-[6px] text-center text-[13px] text-gray-300">
+    return (
+      <div key={`prev-${day}`} className="py-[6px] text-center text-sm font-medium text-gray-300">
         {day}
-      </div>,
+      </div>
     );
-  }
+  });
 
-  // 현재 달의 모든 날짜를 days 배열에 추가하여 HTML구조로 변환시켜줍니다.
-  for (let day = 1; day <= daysInMonth; day += 1) {
-    const date = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`; // YYYY-DD-MM 형식
+  // 현재 달의 날짜 계산
+  const currentDays = Array.from({ length: daysInMonth }, (_, i) => {
+    const day = i + 1;
+    const date = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`; // YYYY-MM-DD 형식
 
-    days.push(
+    return (
       <div
         key={day}
-        className={`cursor-pointer rounded-lg py-[6px] text-center text-[13px] font-semibold ${selectedDate === date && 'bg-orange-600 text-white'}`}
-        onClick={() => handleDateSelection(date)}
+        className={`cursor-pointer rounded-lg py-[6px] text-center text-sm font-semibold ${selectedDate === date ? 'bg-orange-600 text-white' : ''}`}
+        onClick={() => onDateSelect(date)}
       >
         {day}
-      </div>,
+      </div>
     );
-  }
+  });
 
-  let nextMonthDay = 1;
+  // 다음 달의 날짜 계산
+  const nextDaysCount = 35 - (prevDays.length + currentDays.length); // 달력의 셀 개수를 35개로 맞추기 위한 로직
 
-  while (days.length < 35) {
-    days.push(
-      <div key={`next-${nextMonthDay}`} className="py-[6px] text-center text-[13px] text-gray-300">
-        {nextMonthDay}
-      </div>,
+  const nextDays = Array.from({ length: nextDaysCount }, (_, i) => {
+    const day = i + 1;
+
+    return (
+      <div key={`next-${day}`} className="py-[6px] text-center text-sm font-medium text-gray-300">
+        {day}
+      </div>
     );
-    nextMonthDay += 1;
-  }
+  });
 
-  return days;
+  return <>{[...prevDays, ...currentDays, ...nextDays]}</>;
 }
--- src/components/shared/Calendar/index.tsx
+++ src/components/shared/Calendar/index.tsx
@@ -1,6 +1,4 @@
 import Image from 'next/image';
-import Modal from '@/components/shared/Modal';
-import { useModal } from '@/hooks/useModal';
 import { useState } from 'react';
 import RenderCalendar from '@/components/shared/Calendar/RenderCalendar';
 
@@ -10,8 +8,6 @@ export default function Calendar() {
   const [currentDate, setCurrentDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState<string | null>(null);
 
-  const { isOpen, openModal, closeModal } = useModal();
-
   const handleDateSelection = (date: string) => {
     if (selectedDate === date) {
       setSelectedDate(null);
@@ -20,47 +16,40 @@ export default function Calendar() {
     }
   };
 
+  console.log(months[currentDate.getMonth()], currentDate.getFullYear());
+
   return (
-    <div className="flet h-screen items-center justify-center">
-      <button type="button" onClick={openModal} className="flex h-screen w-full items-center justify-center">
-        캘린더열기
-      </button>
-      <Modal isOpen={isOpen} onClose={closeModal}>
-        <div className="p-10">
-          {/* 나중에 제거하기 */}
-          <div className="min-h-[326px] min-w-[336px] rounded-xl border border-gray-200 px-11 py-6">
-            <div className="flex items-center justify-between">
-              <Image
-                src="./assets/icons/left.svg"
-                alt="Previous Btn"
-                width={24}
-                height={24}
-                className="cursor-pointer"
-                onClick={() => setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1))}
-              />
-              <span className="text-sm font-medium text-gray-800">
-                {months[currentDate.getMonth()]} {currentDate.getFullYear()}
-              </span>
-              <Image
-                src="./assets/icons/right.svg"
-                alt="Previous Btn"
-                width={24}
-                height={24}
-                className="cursor-pointer"
-                onClick={() => setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1))}
-              />
-            </div>
-            <div className="mt-3 grid grid-cols-7 gap-[2px]">
-              {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) => (
-                <div key={day} className="text-center text-sm font-semibold text-gray-800">
-                  {day}
-                </div>
-              ))}
-              <RenderCalendar currentDate={currentDate} handleDateSelection={handleDateSelection} selectedDate={selectedDate} />
-            </div>
+    <div className="max-w-[336px] mx-auto">
+      {/* rounded-xl border border-gray-200 px-11 py-6 */}
+      <div className="flex items-center justify-between">
+        <Image
+          src="./assets/icons/left.svg"
+          alt="Previous Btn"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+          onClick={() => setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1))}
+        />
+        <span className="text-sm font-medium text-gray-800">
+          {months[currentDate.getMonth()]} {currentDate.getFullYear()}
+        </span>
+        <Image
+          src="./assets/icons/right.svg"
+          alt="Previous Btn"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+          onClick={() => setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1))}
+        />
+      </div>
+      <div className="mt-3 grid grid-cols-7 gap-[2px]">
+        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) => (
+          <div key={day} className="text-center text-sm font-bold text-gray-800">
+            {day}
           </div>
-        </div>
-      </Modal>
+        ))}
+        <RenderCalendar currentDate={currentDate} onDateSelect={handleDateSelection} selectedDate={selectedDate} />
+      </div>
     </div>
   );
 }
