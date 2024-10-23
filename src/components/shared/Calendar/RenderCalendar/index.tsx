ff7786bfa30f9ff8f8e0af222e078a9bd79ec9b7
eundonghyeok <mephisto7191@naver.com>
2024-10-23 17:48:30 +0900

    Feat: single 모드일 때 이전 날짜 비활성화 기능 추가 및 CSS수정

--- src/components/shared/Calendar/RenderCalendar/index.tsx
+++ src/components/shared/Calendar/RenderCalendar/index.tsx
@@ -1,58 +1,116 @@
-interface CalendarGridProps {
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { useState } from 'react';
+
+const today = new Date();
+
+interface RenderCalendarProps {
   currentDate: Date;
-  onDateSelect: (date: string) => void;
-  selectedDate: string | null;
-  rangeStart: string | null;
   rangeEnd: string | null;
+  rangeStart: string | null;
+  selectedDate: string | null;
   selectionType: 'single' | 'range';
+  onDateSelect: (date: string) => void;
 }
 
-export default function RenderCalendar({ currentDate, onDateSelect, selectedDate, rangeStart, rangeEnd, selectionType }: CalendarGridProps) {
+/**
+ * RenderCalendar 컴포넌트는 달력의 날짜 그리드를 렌더링하고,
+ * 단일 날짜 선택 또는 범위 선택 기능을 제공합니다.
+ *
+ * @param {Date} currentDate - 현재 보고 있는 달의 날짜 객체
+ * @param {string | null} rangeEnd - 범위 선택에서 종료 날짜
+ * @param {string | null} rangeStart - 범위 선택에서 시작 날짜
+ * @param {string | null} selectedDate - 선택된 날짜 ("single" 모드일 경우)
+ * @param {'single' | 'range'} selectionType - 날짜 선택 모드 ("single" or "range")
+ * @param {(date: string) => void} onDateSelect - 날짜 선택 시 호출되는 콜백 함수
+ */
+
+export default function RenderCalendar({ currentDate, onDateSelect, selectedDate, rangeStart, rangeEnd, selectionType }: RenderCalendarProps) {
+  const [hoverDate, setHoverDate] = useState<string | null>(null);
+
   const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(); // 현재 달의 마지막 날
   const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay(); // 현재 달의 1일의 요일
   const prevMonthDays = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate(); // 전달의 마지막 날
 
-  // 전달의 날짜 계산
+  // 전달의 날짜 계산 로직
   const prevDays = Array.from({ length: firstDayOfMonth }, (_, i) => {
-    const day = prevMonthDays - i;
+    const day = prevMonthDays - (firstDayOfMonth - i - 1);
     return (
-      <div key={`prev-${day}`} className="py-[6px] text-center text-sm font-medium text-gray-300">
+      <div key={`prev-${day}`} className="pointer-events-none py-[6px] text-center text-sm font-medium text-gray-300">
         {day}
       </div>
     );
   });
 
-  // 현재 달의 날짜 계산
+  // 현재 달의 날짜 계산 로직
   const currentDays = Array.from({ length: daysInMonth }, (_, i) => {
     const day = i + 1;
     const date = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`; // YYYY-MM-DD 형식
 
-    const today = new Date();
     const todayDateString = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
     const isToday = todayDateString === date;
     const isSelected = selectedDate === date;
     const isInRange = rangeStart && rangeEnd && date >= rangeStart && date <= rangeEnd;
+    const isHoverRange = rangeStart && hoverDate && !rangeEnd && date >= rangeStart && date <= hoverDate;
+
+    const weekDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), day).getDay();
+    const isSunday = weekDay === 0;
+    const isSaturday = weekDay === 6;
+
+    const isPastDate = selectionType === 'single' && new Date(date) < today;
+
+    const textColor = clsx({
+      'text-red-500 font-semibold': isSunday,
+      'text-ocean font-semibold': isSaturday,
+      'text-primary-400 font-bold': isToday,
+      'text-white': !isToday && !isSunday && !isSaturday && (isSelected || isInRange || isHoverRange),
+      'text-gray-300': isPastDate,
+      // 'text-white': !isPastDate && !isToday && !isSunday && !isSaturday && (isSelected || isInRange || isHoverRange),
+    });
+
+    const hoverClasses =
+      selectionType === 'single' || (selectionType === 'range' && !rangeStart) || (selectionType === 'range' && rangeStart && !rangeEnd)
+        ? 'hover:bg-blue-700'
+        : '';
+
+    const dayClasses = twMerge(
+      'cursor-pointer rounded-lg py-[6px] text-center text-sm font-medium transition duration-200 ease-in-out',
+      (isSelected || isInRange) && 'bg-blue-800',
+      isHoverRange && 'bg-blue-700',
+      isPastDate && 'pointer-events-none',
+      textColor,
+      hoverClasses,
+    );
 
     return (
       <div
         key={day}
-        className={`cursor-pointer rounded-lg py-[6px] text-center text-sm font-medium hover:bg-[#3B494F] hover:text-white ${isSelected && 'bg-[#262F33] text-white'} ${isToday && !isSelected && 'text-[#F7C312]'} ${selectionType === 'range' && isInRange ? 'bg-[#262F33] text-white' : ''} }`}
-        onClick={() => onDateSelect(date)}
+        className={dayClasses}
+        onClick={() => {
+          if (rangeStart && !rangeEnd && date >= rangeStart) {
+            onDateSelect(date);
+          } else {
+            setHoverDate(null);
+            onDateSelect(date);
+          }
+        }}
+        onMouseEnter={() => rangeStart && !rangeEnd && setHoverDate(date)}
+        onMouseLeave={() => setHoverDate(null)}
       >
         {day}
       </div>
     );
   });
 
-  // 다음 달의 날짜 계산
-  const nextDaysCount = 35 - (prevDays.length + currentDays.length); // 달력의 셀 개수를 35개로 맞추기 위한 로직입니다.
+  // 다음 달의 날짜 계산 로직
+  const nextDaysCount = 35 - (prevDays.length + currentDays.length); // 달력의 셀 개수 35개 고정로직
 
   const nextDays = Array.from({ length: nextDaysCount }, (_, i) => {
     const day = i + 1;
 
     return (
-      <div key={`next-${day}`} className="py-[6px] text-center text-sm font-medium text-gray-300">
+      <div key={`next-${day}`} className="pointer-events-none py-[6px] text-center text-sm font-medium text-gray-300">
         {day}
       </div>
     );
--- src/components/shared/Calendar/index.tsx
+++ src/components/shared/Calendar/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import RenderCalendar from '@/components/shared/Calendar/RenderCalendar';
+import clsx from 'clsx';
 
 interface CalendarProps {
   selectionType: 'single' | 'range';
@@ -9,15 +10,15 @@ interface CalendarProps {
 /**
  * 달력 컴포넌트 입니다.
  *
- * @param {selectionType} "single" | "range" - 단일 날짜 or 범위 선택 모드
+ * @param {selectionType} "single" | "range" - 단일 날짜(모임 생성) or 범위 선택 모드(모임 찾기)
  */
 
 export default function Calendar({ selectionType }: CalendarProps) {
+  const [dropOpen, setDropOpen] = useState<boolean>(false);
+  const [rangeEnd, setRangeEnd] = useState<string | null>(null);
+  const [rangeStart, setRangeStart] = useState<string | null>(null);
   const [currentDate, setCurrentDate] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState<string | null>(null);
-  const [rangeStart, setRangeStart] = useState<string | null>(null);
-  const [rangeEnd, setRangeEnd] = useState<string | null>(null);
-  const [dropOpen, setDropOpen] = useState<boolean>(false);
 
   const years = Array.from({ length: 10 }, (_, i) => currentDate.getFullYear() - 4 + i);
 
@@ -53,9 +54,9 @@ export default function Calendar({ selectionType }: CalendarProps) {
     <div className="mx-auto size-[250px]">
       <div className="flex items-center justify-between">
         <div className="relative flex w-full items-center justify-between">
-          <span className="flex items-center gap-1 text-sm font-semibold text-gray-800">
+          <span onClick={() => setDropOpen(!dropOpen)} className="flex cursor-pointer items-center gap-1 text-sm font-semibold text-gray-700">
             {currentDate.getFullYear()}년 {currentDate.getMonth() + 1}월{' '}
-            <Image src="./icons/down-arrow.svg" alt="down arrow" width={20} height={20} onClick={() => setDropOpen(!dropOpen)} className="cursor-pointer" />
+            <Image src="./icons/down-arrow.svg" alt="down arrow" width={18} height={18} className={`duration-300 ${dropOpen ? 'rotate-180' : 'rotate-0'}`} />
           </span>
           {dropOpen && (
             <ul
@@ -64,37 +65,43 @@ export default function Calendar({ selectionType }: CalendarProps) {
               }`}
             >
               {years.map((year) => (
-                <li key={year} onClick={() => changeYear(year)} className="cursor-pointer px-2 py-1 text-center hover:bg-gray-100">
+                <li key={year} onClick={() => changeYear(year)} className="cursor-pointer py-1 text-center text-sm hover:bg-gray-50">
                   {year}
                 </li>
               ))}
             </ul>
           )}
-          <div className="flex gap-3">
-            <button type="button" className="relative cursor-pointer" onClick={() => changeMonth('prev')}>
-              <div className="hover:animate-pingpong absolute h-4 w-4 rounded-full hover:bg-[#262F33] hover:opacity-0" />
+          <div className="flex cursor-pointer gap-3">
+            <button type="button" onClick={() => changeMonth('prev')}>
+              <div className="hover:animate-pingpong absolute size-4 rounded-full hover:bg-gray-50 hover:opacity-0" />
               <Image src="./icons/left.svg" alt="Previous Btn" width={16} height={16} />
             </button>
-            <button type="button" className="cursor-pointer" onClick={() => changeMonth('next')}>
-              <div className="hover:animate-pingpong absolute h-4 w-4 rounded-full hover:bg-[#262F33] hover:opacity-0" />
-              <Image src="./icons/right.svg" alt="Previous Btn" width={16} height={16} />
+            <button type="button" onClick={() => changeMonth('next')}>
+              <div className="hover:animate-pingpong absolute size-4 rounded-full hover:bg-gray-50 hover:opacity-0" />
+              <Image src="./icons/right.svg" alt="Next Btn" width={16} height={16} />
             </button>
           </div>
         </div>
       </div>
-      <div className="mt-3 grid grid-cols-7 gap-[2px]">
-        {['일', '월', '화', '수', '목', '금', '토'].map((day) => (
-          <div key={day} className="text-center text-sm font-semibold text-gray-900">
+      <div className="mt-4 grid grid-cols-7">
+        {['일', '월', '화', '수', '목', '금', '토'].map((day, i) => (
+          <div
+            key={day}
+            className={clsx('text-center text-sm font-semibold', {
+              'text-red-500': i === 0,
+              'text-ocean': i === 6,
+            })}
+          >
             {day}
           </div>
         ))}
         <RenderCalendar
-          currentDate={currentDate}
-          onDateSelect={handleDateSelection}
-          selectedDate={selectedDate}
-          rangeStart={rangeStart}
           rangeEnd={rangeEnd}
+          rangeStart={rangeStart}
+          currentDate={currentDate}
+          selectedDate={selectedDate}
           selectionType={selectionType}
+          onDateSelect={handleDateSelection}
         />
       </div>
     </div>
