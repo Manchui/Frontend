4009749af5b41e1fb209e5b2b91391dea2cd3a43
eundonghyeok <mephisto7191@naver.com>
2024-10-25 15:44:45 +0900

    Refactor: 필요없는 더미 파일 삭제

--- src/components/shared/gnbtest/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Gnb() {
-  return <div className="h-[60px]">gnb</div>;
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,7 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import Gnb from '@/components/shared/gnbtest';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Gnb />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
