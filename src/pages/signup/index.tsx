48a29236642ee8d422da592f2da0314405928a6a
YongLeeCode <yonghwan311@gmail.com>
2024-10-25 18:04:16 +0900

    add Carousel component and update signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import instance from '@/apis/api';
+import Carousel from '@/components/Carousel';
 import Input from '@/components/shared/Input';
 
 /**
@@ -47,16 +52,16 @@ export default function SignupPage() {
         password,
         passwordConfirm: passwordCheck,
       });
-    } catch (err) {
-      setError(`error: ${String(err)}`);
+    } catch (err: any) {
+      setError(err);
     }
   };
 
   return (
-    <div className="flex min-h-screen justify-center bg-black">
+    <div className="flex h-screen items-center justify-center bg-black">
       {isDesktop ? (
         // 1240px 이상에서 보일 PC 레이아웃
-        <div className="flex w-full items-center rounded-2xl bg-white">
+        <div className="flex w-full items-center bg-white">
           <div className="flex w-1/2 flex-col items-center space-y-6 p-24">
             <h2 className="m-auto text-4xl font-bold">회원가입</h2>
             <p className="m-auto text-lg">지금 바로 가입하여 특별한 경험을 만들어보세요.</p>
@@ -77,25 +82,21 @@ export default function SignupPage() {
             </form>
             <p className="m-auto mt-4 text-sm">
               이미 회원이신가요?{' '}
-              <Link href="/login" className="text-gray-400 underline">
+              <Link href="/login" className="text-gray-400 underline hover:text-primary-400">
                 로그인
               </Link>
             </p>
           </div>
           <div className="relative flex min-h-screen w-1/2 flex-col items-center justify-center bg-black">
-            <Image src="/images/children-signup.png" className="size-auto" width={300} height={200} alt="nintendo" />
-            <h1 className="text-center text-2xl font-bold text-white">
-              사람들과 함께 <br /> 만취 모임에서 다양한 게임을 즐길 수 있어요.
-            </h1>
-            <p className="text-center text-white">
-              팀을 이뤄 협동 게임에 도전하거나, 간단한 보드게임으로 즐거운 경쟁을 펼쳐보세요. <br />
-              게임을 통해 더 가까워지고, 웃음이 가득한 시간이 기다리고 있습니다.
-            </p>
+            <Carousel />
           </div>
         </div>
       ) : (
         // 1239px 이하에서 보일 모바일 레이아웃
-        <form onClick={handleSignup} className="m-4 flex flex-col items-center rounded-2xl bg-white p-8 mobile:w-3/4 tablet:w-[620px]">
+        <form
+          onClick={handleSignup}
+          className="flex flex-col items-center justify-center bg-white p-8 mobile:max-h-fit mobile:w-full tablet:h-[1000px] tablet:w-[620px] tablet:rounded-2xl"
+        >
           <h2 className="mb-4 text-center text-xl font-bold mobile:text-2xl tablet:text-3xl">회원가입</h2>
           <p className="mb-4 text-center text-sm mobile:text-base tablet:text-lg">지금 바로 가입하여 특별한 경험을 만들어보세요.</p>
           <Image src="/images/children-signup.png" className="size-auto" width={200} height={200} alt="nintendo" />
@@ -116,7 +117,7 @@ export default function SignupPage() {
           </button>
           <p className="mt-4 text-center text-sm mobile:text-base">
             이미 회원이신가요?{' '}
-            <Link href="/login" className="text-gray-400 underline">
+            <Link href="/login" className="text-gray-400 underline hover:text-primary-400">
               로그인
             </Link>
           </p>
