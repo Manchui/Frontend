9ce93fc0308ba9c881b46da5f28eb7df97bd83c0
YongLeeCode <yonghwan311@gmail.com>
2024-10-31 16:44:26 +0900

    feat: 유저 정보 추상화 생성

---
+++ src/apis/userApi.tsx
@@ -0,0 +1,35 @@
+import instance from '@/apis/api';
+
+// eslint-disable-next-line consistent-return
+interface UserInfo {
+  id: string;
+  image: string;
+  name: string;
+}
+
+export const getUserInfo = async (accessToken: string): Promise<UserInfo | undefined> => {
+  try {
+    const res = await instance.get<UserInfo>('http://localhost:3010/user', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    return undefined;
+  }
+};
+
+export const logout = async () => {
+  try {
+    await instance.post('http://localhost:3011/logout');
+    localStorage.removeItem('accessToken');
+    document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    window.location.reload();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Logout failed:', error);
+  }
+};
