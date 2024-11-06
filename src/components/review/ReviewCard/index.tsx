913330cee5f9b3b752d7ae65409bfc72de9a12d8
inji0212 <in17100170@kakao.com>
2024-11-06 14:34:27 +0900

    feat 1차 api연동( 페이지네이션 , sort x)

--- src/components/review/ReviewCard/index.tsx
+++ src/components/review/ReviewCard/index.tsx
@@ -1,22 +1,16 @@
 import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Rating from '@/components/shared/Rating';
+import { ReviewContentList } from '@/types/detail';
+import type { GetReviewResponse } from '@manchui-api';
 
 import MoreButton from './MoreButton';
 
-type ReviewList = {
-  ImagePath: string;
-  comment: string;
-  createdAt: string;
-  gatheringLocation: string;
-  gatheringName: string;
-  name: string;
-  profileImagePath: string;
-  score: number;
-  userId: string;
+type ReviewCardSProps = {
+  review: GetReviewResponse['data']['reviewContentList'][number];
 };
 
-export function ReviewCard({ review }: { review: ReviewList }) {
+export function ReviewCard({ review }: ReviewCardSProps) {
   const dateObj = new Date(review.createdAt);
   const [isExpanded, setIsExpanded] = useState(false);
   const [showMoreButton, setShowMoreButton] = useState(false);
@@ -39,7 +33,7 @@ export function ReviewCard({ review }: { review: ReviewList }) {
 
       const handleResize = () => {
         checkClamped();
-        setIsExpanded(false); 
+        setIsExpanded(false);
       };
 
       window.addEventListener('resize', handleResize);
@@ -51,14 +45,18 @@ export function ReviewCard({ review }: { review: ReviewList }) {
 
   return (
     <div className="w-[280px] flex-col items-start gap-4 tablet:flex tablet:w-full tablet:flex-row">
-      <Image
-        alt="testImage"
-        src={review.ImagePath}
-        width={280}
-        height={156}
-        style={{ objectFit: 'cover' }}
-        className="relative mx-auto flex-shrink-0 overflow-hidden rounded-lg"
-      />
+      {review.gatheringImage ? (
+        <Image
+          alt="testImage"
+          src={review.gatheringImage}
+          width={280}
+          height={156}
+          style={{ width: '280px', height: '156px', objectFit: 'cover' }}
+          className="relative mx-auto flex-shrink-0 overflow-hidden rounded-lg"
+        />
+      ) : (
+        <div className="relative mx-auto h-[156px] w-[280px] flex-shrink-0 overflow-hidden rounded-lg bg-gray-200" />
+      )}
       <div className="flex flex-grow flex-col items-start justify-start gap-2 border-b-2 border-dashed border-gray-50 pt-4 tablet:min-h-[156px] tablet:pt-0">
         <Rating score={review.score} />
         <div className="flex w-full flex-col items-start">
@@ -69,13 +67,20 @@ export function ReviewCard({ review }: { review: ReviewList }) {
           {showMoreButton && <MoreButton isExpanded={isExpanded} onClick={toggleExpand} />}
         </div>
         <div className="flex w-full items-center gap-1 text-xs font-medium text-blue-700">
-          <span>{review.gatheringName}</span>
+          <span>{review.groupName}</span>
           <span> &bull;</span>
-          <span>{review.gatheringLocation}</span>
+          <span>{review.location}</span>
         </div>
         <div className="mb-3 flex items-center gap-2 text-xs font-medium">
-          <div className="relative size-6 rounded-full">
-            <Image alt="profile" src={review.profileImagePath} fill style={{ objectFit: 'cover' }} className="rounded-full" />
+          <div className="relative">
+            <Image
+              alt="profile"
+              src={review.profileImagePth ? review.profileImagePth : 'images/profile.svg'}
+              width={24}
+              height={24}
+              style={{ objectFit: 'cover' }}
+              className="rounded-full"
+            />
           </div>
           <span className="text-blue-700">{review.name}</span>
           <span>|</span>
--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -1,11 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-shadow */
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getReviewData } from '@/apis/getReviewData';
 import FilterSection from '@/components/review/FilterSection';
 import HeaderSection from '@/components/review/HeaderSection';
+import MainHeader from '@/components/review/HeaderSection/MainHeader';
 import { ReviewCard } from '@/components/review/ReviewCard';
 import MainContainer from '@/components/review/ReviewContainer';
 import Pagination from '@/components/shared/pagination';
 import RootLayout from '@/components/shared/RootLayout';
+import { FILTER_OPTIONS } from '@/constants/contants';
+import type { GetReviewResponse } from '@manchui-api';
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
 
 type ReviewType = {
   ImagePath: string;
@@ -18,72 +26,113 @@ type ReviewType = {
   score: number;
   userId: string;
 };
-const mockData: ReviewType[] = Array.from({ length: 100 }, (_, i) => ({
-  comment: `리뷰 내용 ${i + 1} - ${'123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'}`,
-  createdAt: '2024-01-25',
-  name: `사용자 ${i + 1}`,
-  ImagePath: '/images/test-detail.png',
-  profileImagePath: '/images/profile.svg',
-  score: Math.floor(Math.random() * 5) + 1,
-  userId: `${i + 1}`,
-  gatheringName: '모르는 개 산책',
-  gatheringLocation: '이태원',
-}));
 
-export default function Home() {
-  const [searchValue, setSearchValue] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState<string>('전체');
-  const [data, setData] = useState<ReviewType[]>([]);
+export default function ReviewPage() {
+  const router = useRouter();
+  const [keyword, setKeyword] = useState<string | undefined>((router.query.keyword as string | undefined) || undefined);
+  const [region, setRegion] = useState<string | undefined>((router.query.category as string | undefined) || undefined);
+  const [category, setCategory] = useState<string | undefined>((router.query.category as string | undefined) || FILTER_OPTIONS[0].id);
+  const [closeDate, setCloseDate] = useState<string | undefined>(undefined);
+  const [dateStart, setDateStart] = useState<string | undefined>(undefined);
+  const [dateEnd, setDateEnd] = useState<string | undefined>(undefined);
+
+  const [, setData] = useState<ReviewType[]>([]);
   const [page, setPage] = useState(0);
-  const [size, ] = useState(10);
+  const [pagesize] = useState(10);
   const [totalPage, setTotalPage] = useState(0);
 
-  const handleCategoryClick = (category: string) => {
-    setSelectedCategory(category);
+  const {
+    data: reviewData,
+
+    // isLoading,
+  } = useQuery<GetReviewResponse>({
+    queryKey: [
+      'review',
+      {
+        size: pagesize,
+        query: keyword,
+        location: region,
+        category,
+        sort: closeDate,
+        startDate: dateStart,
+        endDate: dateEnd,
+      },
+    ],
+    queryFn: ({ pageParam = 0 }) =>
+      getReviewData({
+        page: (pageParam as number) + 1,
+        size: pagesize,
+        query: keyword,
+        location: region,
+        startDate: dateStart,
+        endDate: dateEnd,
+        sort: closeDate,
+        category,
+      }),
+  });
+
+  const handleCategoryClick = (selectedCategory: string) => {
+    setCategory(selectedCategory);
   };
 
-  const fetchData = (page: number, size: number) => {
-    const start = page * size;
-    const end = start + size;
-    const slicedData = mockData.slice(start, end);
-
-    setData(slicedData);
-    setTotalPage(Math.ceil(mockData.length / size));
+  const handleSearchSubmit = (submitValue: string) => {
+    setKeyword(submitValue);
   };
 
-  useEffect(() => {
-    fetchData(page, size);
-  }, [page, size]);
+  const handleCloseDateClick = (value: string) => {
+    setCloseDate(value);
+  };
+
+  const handleDateSubmit = ({ start, end }: { end: string; start: string }) => {
+    setDateStart(start);
+    setDateEnd(end);
+  };
+
+  // const fetchData = (page: number, size: number) => {
+  //   const start = page * size;
+  //   const end = start + size;
+  //   const slicedData = mockData.slice(start, end);
+
+  //   setData(slicedData);
+  //   setTotalPage(Math.ceil(mockData.length / size));
+  // };
+
+  // useEffect(() => {
+  //   fetchData(page, pagesize);
+  // }, [page, pagesize]);
 
   return (
     <div className="mt-[60px]">
+      <MainHeader />
       <RootLayout>
-        <div className="flex h-[155px] w-full flex-col items-center justify-center gap-[10px] bg-blue-800 text-white">
-          <p className="text-2xl font-bold">모든 리뷰</p>
-          <span className="text-base font-medium">만취를 이용한 분들은 이렇게 느꼈어요</span>
-        </div>
         <MainContainer>
           {/* Header (타이틀, 검색창) */}
-          <HeaderSection searchValue={searchValue} setSearchValue={setSearchValue} selectedCategory={selectedCategory} />
+          <HeaderSection keyword={keyword} category={category} handleSearchSubmit={handleSearchSubmit} />
 
           {/* 카테고리 */}
-          <FilterSection selectedCategory={selectedCategory} handleCategoryClick={handleCategoryClick} />
+          <FilterSection
+            region={region}
+            category={category}
+            setRegion={setRegion}
+            setDateEnd={setDateEnd}
+            setDateStart={setDateStart}
+            handleDateSubmit={handleDateSubmit}
+            handleCategoryClick={handleCategoryClick}
+            handleCloseDateClick={handleCloseDateClick}
+          />
           {/* 카드 */}
           <section className="mt-0 flex w-full flex-col items-center gap-6 bg-white px-4 pb-6 pt-1 mobile:rounded-lg tablet:items-start">
-            {data.map((item) => (
-              <div key={item.userId}>
-                <ReviewCard review={item} />
-              </div>
-            ))}
+            {reviewData?.data.reviewContentList.map((reviewContent) => <ReviewCard key={reviewContent.reviewId} review={reviewContent} />)}
+
             <div className="flex self-center">
               <Pagination
                 page={page}
-                
                 totalPage={totalPage}
-                setPage={setPage} 
+                setPage={setPage}
                 // 임시
               />
             </div>
+            {!reviewData && <div className="h-20 w-full">작성된 리뷰가 없습니다.</div>}
           </section>
         </MainContainer>
       </RootLayout>
