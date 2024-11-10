9fd5bbfff22ee8d525f24d99a367f9f6f7ff1380
inji0212 <in17100170@kakao.com>
2024-11-11 08:37:01 +0900

    fix review page bookmark page와 동일한 레이아웃으로 수정

--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -2,13 +2,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import Lottie from 'lottie-react';
-import { useRouter } from 'next/router';
 import { getReviewData } from '@/apis/getReviewData';
+import BookmarkContainer from '@/components/bookmark/BookmarkContainer';
 import HeaderSection from '@/components/main/HeaderSection';
 import FilterSection from '@/components/review/FilterSection';
 import MainHeader from '@/components/review/MainHeader';
 import ReviewCardList from '@/components/review/ReviewCardList';
-import ReviewContainer from '@/components/review/ReviewContainer';
 import PaginationBtn from '@/components/shared/PaginationBtn';
 import RootLayout from '@/components/shared/RootLayout';
 import { FILTER_OPTIONS } from '@/constants/contants';
@@ -16,7 +15,6 @@ import useGetReviewData from '@/hooks/useGetReviewData';
 import { dehydrate, QueryClient } from '@tanstack/react-query';
 
 export default function ReviewPage() {
-  const router = useRouter();
   const [page, setPage] = useState(1);
   const [keyword, setKeyword] = useState<string | undefined>(undefined);
   const [sort, setSort] = useState<string | undefined>(undefined);
@@ -69,29 +67,32 @@ export default function ReviewPage() {
         <MainHeader />
       )}
       <RootLayout>
-        <ReviewContainer>
+        <BookmarkContainer>
           {/* Header (타이틀, 검색창) */}
           <HeaderSection keyword={keyword} category={category} handleSearchSubmit={handleSearchSubmit} />
-          {/* 카테고리 */}
-          <FilterSection
-            location={location}
-            category={category}
-            sort={sort}
-            setSort={setSort}
-            setDateEnd={setDateEnd}
-            setLocation={setLocation}
-            setDateStart={setDateStart}
-            handleDateSubmit={handleDateSubmit}
-            handleCategoryClick={handleCategoryClick}
-          />
-          {/* 카드 */}
-          <ReviewCardList data={reviewData?.data} isLoading={isLoading} isError={isError} />
-          {!isLoading && !isError && reviewData?.data?.reviewContentList && reviewData.data.reviewContentList.length > 0 && (
-            <div className="w-full bg-white">
-              <PaginationBtn page={reviewData?.data.page ?? 0} totalPage={reviewData?.data.totalPage ?? 0} handlePageChange={handlePageChange} />
-            </div>
-          )}
-        </ReviewContainer>
+          <div className="mt-2 min-h-screen w-full rounded-t-lg bg-white">
+            {/* 카테고리 */}
+            <FilterSection
+              location={location}
+              category={category}
+              sort={sort}
+              setSort={setSort}
+              setDateEnd={setDateEnd}
+              setLocation={setLocation}
+              setDateStart={setDateStart}
+              handleDateSubmit={handleDateSubmit}
+              handleCategoryClick={handleCategoryClick}
+            />
+            {/* 카드 */}
+            <ReviewCardList data={reviewData?.data} isLoading={isLoading} isError={isError} />
+
+            {!isLoading && !isError && reviewData?.data?.reviewContentList && reviewData.data.reviewContentList.length > 0 && (
+              <div className="w-full bg-white">
+                <PaginationBtn page={reviewData?.data.page ?? 0} totalPage={reviewData?.data.totalPage ?? 0} handlePageChange={handlePageChange} />
+              </div>
+            )}
+          </div>
+        </BookmarkContainer>
       </RootLayout>
     </div>
   );
