6d21d479589746b820a428330216e1cfd0b333f3
YongLeeCode <yonghwan311@gmail.com>
2024-11-13 18:45:48 +0900

    refactor: 로그인 후 하트 버튼 버그 수정

--- src/components/main/CardSection/CardContent/index.tsx
+++ src/components/main/CardSection/CardContent/index.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable tailwindcss/no-custom-classname */
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import instance from '@/apis/api';
 import DateChip from '@/components/shared/chip/DateChip';
 import { ProgressBar } from '@/components/shared/progress-bar';
 import { Toast } from '@/components/shared/Toast';
-import { IS_SERVER } from '@/constants/server';
+import { userStore } from '@/store/userStore';
 import type { GetGatheringResponse } from '@manchui-api';
 import { useQueryClient } from '@tanstack/react-query';
 
@@ -17,8 +17,7 @@ interface CardContentProps {
 export default function CardContent({ gathering }: CardContentProps) {
   const { hearted, gatheringId, groupName, gatheringDate, closed, location, maxUsers, minUsers, currentUsers } = gathering;
   const [isHearted, setIsHearted] = useState(hearted);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
+  const isLoggedIn = userStore((state) => state.isLoggedIn);
   const queryClient = useQueryClient();
 
   const toggleHeart = async () => {
@@ -26,20 +25,17 @@ export default function CardContent({ gathering }: CardContentProps) {
       Toast('warning', '로그인이 필요합니다.');
       return;
     }
-
-    const updatedHearted = !isHearted;
-    setIsHearted(updatedHearted);
+    
     const endpoint = `/api/gatherings/${gatheringId}/heart`;
-
     try {
-      if (updatedHearted) {
+      if (!hearted) {
         await instance.post(endpoint);
         Toast('success', '찜 목록에 추가되었습니다!');
       } else {
         await instance.delete(endpoint);
         Toast('success', '찜 목록에서 제거되었습니다!');
       }
-
+      setIsHearted(!isHearted);
       await queryClient.invalidateQueries({ queryKey: ['main'] });
       await queryClient.invalidateQueries({ queryKey: ['bookmark'] });
     } catch (e) {
@@ -48,14 +44,8 @@ export default function CardContent({ gathering }: CardContentProps) {
     }
   };
 
-  useEffect(() => {
-    if (!IS_SERVER) {
-      setIsLoggedIn(!!localStorage.getItem('accessToken'));
-    }
-  }, []);
-
   return (
-    <div className="border-t-cardBorder mobile:border-cardBorder relative flex h-1/2 flex-1 flex-col justify-between rounded-2xl rounded-t-none border-t p-2 mobile:h-full mobile:w-1/2 mobile:rounded-bl-none mobile:border-l mobile:p-4 tablet:h-1/2 tablet:w-full tablet:border-t tablet:border-none">
+    <div className="relative flex h-1/2 flex-1 flex-col justify-between rounded-2xl rounded-t-none border-t border-t-cardBorder p-2 mobile:h-full mobile:w-1/2 mobile:rounded-bl-none mobile:border-l mobile:border-cardBorder mobile:p-4 tablet:h-1/2 tablet:w-full tablet:border-t tablet:border-none">
       <Link href={`/detail/${gatheringId}`} className="my-auto flex flex-col gap-1">
         <div className={`mb-2 flex flex-col tablet:mb-0 ${closed ? 'text-gray-200' : 'text-black'}`}>
           <span className="text-pretty text-16-20-response font-semibold">{groupName}</span>
