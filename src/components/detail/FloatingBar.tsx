d572de76b54840af25510768c5cc4fe7fcd669cd
sohee <topa11uu@gmail.com>
2024-11-13 18:39:06 +0900

    Docs: 버튼 조건 추가

--- src/components/detail/FloatingBar.tsx
+++ src/components/detail/FloatingBar.tsx
@@ -19,7 +19,8 @@ interface FloatingBarProps extends DetailPageBaseType {
 export function FloatingBar({ gatherings, id, usersList, maxUsers }: FloatingBarProps) {
   const myUserName = userStore((state) => state.user.name);
   const findUserId = usersList.find((user) => user.name === myUserName);
-  const isDisabled = usersList.length === maxUsers || gatherings.closed;
+  const isDisabled = usersList.length === maxUsers;
+  const isClosed = gatherings.closed;
 
   return (
     <footer className="fixed inset-x-0 bottom-0 flex min-h-[84px] items-center justify-between border-t bg-white px-10 py-5">
@@ -27,7 +28,9 @@ export function FloatingBar({ gatherings, id, usersList, maxUsers }: FloatingBar
         <span className="text-base font-semibold text-[#111827]">더 건강한 나와 팀을 위한 프로그램 🏃‍️️</span>
         <span className="text-sm font-medium text-[#111827]">프로그램을 통해 지친 몸과 마음을 회복해봐요</span>
       </div>
-      {findUserId ? (
+      {isClosed ? (
+        <Button label="마감되었습니다" size="primary" variant="primary" disabled />
+      ) : findUserId ? (
         <div className="flex gap-2">
           <CancelButton gatherings={gatherings} id={id} />
           <ShareButton />
