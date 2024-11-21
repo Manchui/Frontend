d7f332df65264c675c3507c316fd8fbb350c48c1
sohee <topa11uu@gmail.com>
2024-11-21 17:06:54 +0900

    Docs: 상세 페이지 코드 수정

--- src/pages/detail/[id]/index.tsx
+++ src/pages/detail/[id]/index.tsx
@@ -5,20 +5,26 @@ import { ReviewListCard } from '@/components/detail/ReviewListCard';
 import Score from '@/components/detail/score';
 import RootLayout from '@/components/shared/RootLayout';
 import { SEO } from '@/components/shared/SEO';
-import type { DetailData } from '@/types/detail';
 import type { DehydratedState } from '@tanstack/react-query';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { dehydrate, HydrationBoundary, QueryClient, useQuery } from '@tanstack/react-query';
 
 interface DetailPageProps {
   dehydratedState: DehydratedState;
-  gatherings: DetailData;
   isId: number;
   seo: {
     title: string;
   };
 }
 
-export default function DetailPage({ seo, dehydratedState, gatherings, isId }: DetailPageProps) {
+export default function DetailPage({ seo, dehydratedState, isId }: DetailPageProps) {
+  const { data: gatherings } = useQuery({
+    // NOTE: page,size는 임시값
+    queryKey: ['detail', { isId, page: 1, size: 10 }],
+    queryFn: () => getGatheringData(isId),
+    staleTime: 1000 * 10,
+  });
+  if (!gatherings) return null;
+
   return (
     <>
       <SEO title={seo.title} />
@@ -40,11 +46,9 @@ export const getServerSideProps = async (context: { params: { id: number } }) =>
   const queryClient = new QueryClient();
   const { id } = context.params;
 
-  const gatherings = await getGatheringData(id);
-
   await queryClient.prefetchQuery({
     queryKey: ['detail', { id, page: 1, size: 10 }],
-    queryFn: () => gatherings,
+    queryFn: () => getGatheringData(id),
   });
 
   return {
@@ -53,7 +57,6 @@ export const getServerSideProps = async (context: { params: { id: number } }) =>
       seo: {
         title: '상세 페이지',
       },
-      gatherings,
       isId: id,
     },
   };
