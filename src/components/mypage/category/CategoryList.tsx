dbb2e071d43c24e627e129a395e642d90d6194a6
sohee <topa11uu@gmail.com>
2024-11-22 10:22:15 +0900

    Docs: 시맨틱 태그 추가 및 코드 수정

--- src/components/mypage/category/CategoryList.tsx
+++ src/components/mypage/category/CategoryList.tsx
@@ -33,22 +33,19 @@ export default function MyPageCategoryList({ category, setCategory }: MyPageCate
     categoryId === query.category || categoryId === category ? 'flex-1 border-blue-800' : 'flex-1 hover:text-gray-500 text-blue-400 border-b-2 border-blue-100';
 
   return (
-    <div>
-      <nav className="flex select-none items-center justify-between text-sub-response font-semibold">
-        {categories.map((item) => (
-          <button
-            key={item}
-            type="button"
-            onClick={() => {
-              handleCategoryChange(item);
-            }}
-            className={`${getButtonClass(item)}`}
-          >
-            <div className="py-1.5">{item}</div>
-            {item === selectedTab ? <m.div className="border-b-2 border-blue-800" layoutId="underline" /> : null}
-          </button>
-        ))}
-      </nav>
-    </div>
+    <article className="flex select-none items-center justify-between text-center text-sub-response font-semibold">
+      {categories.map((item) => (
+        <ul
+          key={item}
+          onClick={() => {
+            handleCategoryChange(item);
+          }}
+          className={`${getButtonClass(item)}`}
+        >
+          <div className="py-1.5">{item}</div>
+          {item === selectedTab ? <m.div className="border-b-2 border-blue-800" layoutId="underline" /> : null}
+        </ul>
+      ))}
+    </article>
   );
 }
